497a19573f15dcda52107268985d394b3c2ddb8b
Danny Stirling <Daniel>
2021-10-03 19:46:51 -0400

    time changes test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[allow(non_snake_case)]
 extern crate sdl2;
 
 use sdl2::event::Event;
@@ -8,13 +9,23 @@ use sdl2::keyboard::Keycode;
 const TITLE: &str = "Roguelike Credits";
 const CAM_W: u32 = 1280;
 const CAM_H: u32 = 720;
-const TIMEOUT: u64 = 4500;
+const TIMEOUT: u64 = 2500;
 
 pub fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
 
-    
+    let window = video_subsystem
+        .window(TITLE, CAM_W, CAM_H)
+        .position_centered()
+        .opengl()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
+    let texture_creator = canvas.texture_creator();
+
+    'credits_loop: loop {
         for event in sdl_context.event_pump()?.poll_iter() {
             match event {
                 // These keypresses aren't working...can't figure out why!
@@ -28,7 +39,7 @@ pub fn main() -> Result<(), String> {
             let mut texture = texture_creator.load_texture("images/credits/credits_title.png")?;
             canvas.copy(&texture, None, None)?;
             canvas.present();
-            ::std::thread::sleep(Duration::from_millis(TIMEOUT));
+            ::std::thread::sleep(Duration::from_millis(TIMEOUT+1500));
     
             // Maybe split up people by teams
 
