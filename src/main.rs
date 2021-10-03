c155d76a5cdb53cc467fcc2dc71de197b5b4725f
MAL341 <mal341@pitt.edu>
2021-10-03 19:35:26 -0400

    Fix user input.

--- src/main.rs
+++ src/main.rs
@@ -5,81 +5,79 @@ use std::time::Duration;
 use sdl2::image::LoadTexture;
 use sdl2::keyboard::Keycode;
 
-// use sdl_rust::SDLCore;
-// use sdl_rust::Demo;
-
 const TITLE: &str = "Roguelike Credits";
 const CAM_W: u32 = 1280;
 const CAM_H: u32 = 720;
 const TIMEOUT: u64 = 4500;
 
-/*pub struct CREDITS {
-	core: SDLCore,
-}*/
-
-// {
-    // fn init() -> Result<(), String> {
-        // let core = SDLCore::init(TITLE, true, CAM_W, CAM_H)?;
-        // Ok(CREDITS{ core })
-    // }
-
 fn run() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
 
-    
+    let window = video_subsystem
+        .window(TITLE, CAM_W, CAM_H)
+        .position_centered()
+        .opengl()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
+    let texture_creator = canvas.texture_creator();
+
+    let mut i = 0;
+    'credits_loop: loop {
         for event in sdl_context.event_pump()?.poll_iter() {
             match event {
-                // These keypresses aren't working...can't figure out why!
-                // Event::Quit{..} | Event::KeyDown{keycode: Some(Keycode::Escape), ..} => break 'credits_loop,
-                // Event::KeyDown{keycode: Some(Keycode::Q), ..} => break 'credits_loop,
-		Event::Quit{..} | Event::KeyDown{keycode: Some(Keycode::Escape), ..} => break 'credits_loop,
+                Event::Quit{..} | Event::KeyDown{keycode: Some(Keycode::Escape), ..} => break 'credits_loop,
+                Event::KeyDown{keycode: Some(Keycode::Q), ..} => break 'credits_loop,
                 _ => {},
             }
-	}
-            // Copy image texture to canvas, present, timeout
-    
-            // Title
-            let mut texture = texture_creator.load_texture("images/credits/credits_title.png")?;
-            canvas.copy(&texture, None, None)?;
-            canvas.present();
-            std::thread::sleep(Duration::from_millis(TIMEOUT));
-    
-            // Maybe split up people by teams
+        }
+        // Copy image texture to canvas, present, timeout
 
-            // Davon
-            texture = texture_creator.load_texture("images/credits/credits_davon.png")?;
-            canvas.copy(&texture, None, None)?;
-            canvas.present();
-            std::thread::sleep(Duration::from_millis(TIMEOUT));
+	let mut texture = texture_creator.load_texture("images/credits/credits_title.png")?;
 
-            // Daniel 
-            texture = texture_creator.load_texture("images/credits/credits_daniel.png")?;
-            canvas.copy(&texture, None, None)?;
-            canvas.present();
-            std::thread::sleep(Duration::from_millis(TIMEOUT));
+        // Title
+        match i {
+            i if i < 200 => {
+                texture = texture_creator.load_texture("images/credits/credits_title.png")?;
+                canvas.copy(&texture, None, None)?;
+                canvas.present();
+            }
+            i if i < 400 => {
+                texture = texture_creator.load_texture("images/credits/credits_davon.png")?;
+                canvas.copy(&texture, None, None)?;
+                canvas.present();
+            }
+            i if i < 600 => {
+                texture = texture_creator.load_texture("images/credits/credits_daniel.png")?;
+                canvas.copy(&texture, None, None)?;
+                canvas.present();
+            }
+            i if i < 800 => {
+                texture = texture_creator.load_texture("images/credits/credits_victor.png")?;
+                canvas.copy(&texture, None, None)?;
+                canvas.present();
+            }
+            i if i < 1000 => {
+                texture = texture_creator.load_texture("images/credits/credits_adam.png")?;
+                canvas.copy(&texture, None, None)?;
+                canvas.present();
+            }
+            _ => {
+                texture = texture_creator.load_texture("images/credits/credits_title.png")?;
+            }
+        }
 
-            //Victor
-            texture = texture_creator.load_texture("images/credits/credits_victor.png")?;
-            canvas.copy(&texture, None, None)?;
-            canvas.present();
-            std::thread::sleep(Duration::from_millis(TIMEOUT));
+	i += 1;
 
-            //Adam
-            texture = texture_creator.load_texture("images/credits/credits_adam.png")?;
-            canvas.copy(&texture, None, None)?;
-            canvas.present();
-            std::thread::sleep(Duration::from_millis(TIMEOUT));
-
-            break 'credits_loop;
-        // }
+	if(i > 1000) {
+            i = 0;
+        }
     }
-
     Ok(())
 }
 
 fn main() {
-    // sdl_rust::runner(TITLE, CREDITS::init);
-    // init();
     run();
 }
