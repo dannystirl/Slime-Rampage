8bbe549b2338286b1a6b5e4c0ee72b43933831b5
noofster <amw298@pitt.edu>
2021-10-09 14:58:02 -0400

    Add example core and prof movement code

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,116 @@
-extern crate sdl2;
+extern crate rogue_sdl;
 
 mod credits;
 
+use std::collections::HashSet;
+
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
 use sdl2::event::Event;
-use sdl2::image::LoadTexture;
 use sdl2::keyboard::Keycode;
 
-const TITLE: &str = "Roguelike Credits";
-const CAM_W: u32 = 1280;
-const CAM_H: u32 = 720;
-const FRAME_GAP: u32 = 200;
+use rogue_sdl::SDLCore;
+use rogue_sdl::Game;
+
+const TITLE: &str = "SDL07 Key Events";
+const CAM_W: u32 = 640;
+const CAM_H: u32 = 480;
+const SPEED_LIMIT: i32 = 5;
+const ACCEL_RATE: i32 = 1;
+
+fn resist(vel: i32, deltav: i32) -> i32 {
+	if deltav == 0 {
+		if vel > 0 {
+			-1
+		}
+		else if vel < 0 {
+			1
+		}
+		else {
+			deltav
+		}
+	}
+	else {
+		deltav
+	}
+}
+
+pub struct SDL07 {
+	core: SDLCore,
+}
+
+impl Game for SDL07 {
+	fn init() -> Result<Self, String> {
+		let core = SDLCore::init(TITLE, true, CAM_W, CAM_H)?;
+		Ok(SDL07{ core })
+	}
+
+	fn run(&mut self) -> Result<(), String> {
+		let w = 25;
+		let mut x_pos = (CAM_W/2 - w/2) as i32;
+		let mut y_pos = (CAM_H/2 - w/2) as i32;
+
+		let mut x_vel = 0;
+		let mut y_vel = 0;
+
+		'gameloop: loop {
+			for event in self.core.event_pump.poll_iter() {
+				match event {
+					Event::Quit{..} | Event::KeyDown{keycode: Some(Keycode::Escape), ..} => break 'gameloop,
+					_ => {},
+				}
+			}
+
+			let keystate: HashSet<Keycode> = self.core.event_pump
+				.keyboard_state()
+				.pressed_scancodes()
+				.filter_map(Keycode::from_scancode)
+				.collect();
+
+			let mut x_deltav = 0;
+			let mut y_deltav = 0;
+			if keystate.contains(&Keycode::W) {
+				y_deltav -= ACCEL_RATE;
+			}
+			if keystate.contains(&Keycode::A) {
+				x_deltav -= ACCEL_RATE;
+			}
+			if keystate.contains(&Keycode::S) {
+				y_deltav += ACCEL_RATE;
+			}
+			if keystate.contains(&Keycode::D) {
+				x_deltav += ACCEL_RATE;
+			}
+
+			// Slow down to 0 vel if no input and non-zero velocity
+			x_deltav = resist(x_vel, x_deltav);
+			y_deltav = resist(y_vel, y_deltav);
+
+			// Don't exceed speed limit
+			x_vel = (x_vel + x_deltav).clamp(-SPEED_LIMIT, SPEED_LIMIT);
+			y_vel = (y_vel + y_deltav).clamp(-SPEED_LIMIT, SPEED_LIMIT);
+
+			// Stay inside the viewing window
+			x_pos = (x_pos + x_vel).clamp(0, (CAM_W - w) as i32);
+			y_pos = (y_pos + y_vel).clamp(0, (CAM_H - w) as i32);
+
+			self.core.wincan.set_draw_color(Color::BLACK);
+			self.core.wincan.clear();
+
+			self.core.wincan.set_draw_color(Color::CYAN);
+			self.core.wincan.fill_rect(Rect::new(x_pos, y_pos, w, w))?;
+
+			self.core.wincan.present();
+		}
+
+		// Out of game loop, return Ok
+		Ok(())
+	}
+}
 
 pub fn main() -> Result<(), String> {
+
+    rogue_sdl::runner(TITLE, SDL07::init);
     credits::run_credits();
     Ok(())
 }
