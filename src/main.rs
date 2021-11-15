d1b2dbd66a24290dc5d38486df49ba7d9333b12d
noofster <amw298@pitt.edu>
2021-11-14 21:52:48 -0500

    music working

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 extern crate rogue_sdl;
 use rogue_sdl::{Game, SDLCore};
-
+use sdl2::audio::AudioSpecDesired;
+use sdl2::audio::AudioSpecWAV;
+use sdl2::audio::AudioCVT;
 use std::time::Duration;
 use std::time::Instant;
+use sdl2::audio::AudioCallback;
 //use std::cmp;
 use std::collections::HashSet;
 use sdl2::event::Event;
@@ -13,7 +16,9 @@ use sdl2::image::LoadTexture;
 use sdl2::render::{Texture};//,TextureCreator};
 //use sdl2::pixels::Color;
 use rand::Rng;
-
+use sdl2::mixer::{InitFlag, AUDIO_S16LSB, DEFAULT_CHANNELS};
+use std::env;
+use std::path::Path;
 mod background;
 mod credits;
 mod enemy;
@@ -36,7 +41,34 @@ use crate::projectile::*;
 //use crate::room::*;
 //use crate::ui::*;
 //use crate::crateobj::*;
+struct Sound {
+    data: Vec<u8>,
+    volume: f32,
+    pos: usize,
+}
+impl AudioCallback for Sound {
+    type Channel = u8;
 
+    fn callback(&mut self, out: &mut [u8]) {
+        for dst in out.iter_mut() {
+            // With channel type u8 the "silence" value is 128 (middle of the 0-2^8 range) so we need
+            // to both fill in the silence and scale the wav data accordingly. Filling the silence
+            // once the wav is finished is trivial, applying the volume is more tricky. We need to:
+            // * Change the range of the values from [0, 255] to [-128, 127] so we can multiply
+            // * Apply the volume by multiplying, this gives us range [-128*volume, 127*volume]
+            // * Move the resulting range to a range centered around the value 128, the final range
+            //   is [128 - 128*volume, 128 + 127*volume] – scaled and correctly positioned
+            //
+            // Using value 0 instead of 128 would result in clicking. Scaling by simply multiplying
+            // would not give correct results.
+            let pre_scale = *self.data.get(self.pos).unwrap_or(&128);
+            let scaled_signed_float = (pre_scale as f32 - 128.0) * self.volume;
+            let scaled = (scaled_signed_float + 128.0) as u8;
+            *dst = scaled;
+            self.pos += 1;
+        }
+    }
+}
 pub struct ROGUELIKE {
 	core: SDLCore,
 	game_data: GameData,
@@ -54,6 +86,45 @@ impl Game for ROGUELIKE  {
 	fn run(&mut self) -> Result<(), String> {
         let texture_creator = self.core.wincan.texture_creator();
 		let mut rng = rand::thread_rng();
+
+		let audio_subsystem = self.core.sdl_cxt.audio()?;
+		let mut timer = self.core.sdl_cxt.timer()?;
+
+		let frequency = 44_100;
+		let format = AUDIO_S16LSB; // signed 16 bit samples, in little-endian byte order
+		let channels = DEFAULT_CHANNELS; // Stereo
+		let chunk_size = 1_024;
+		sdl2::mixer::open_audio(frequency, format, channels, chunk_size)?;
+		let _mixer_context =
+			sdl2::mixer::init(InitFlag::MP3 | InitFlag::FLAC | InitFlag::MOD | InitFlag::OGG)?;
+	
+		    sdl2::mixer::allocate_channels(4);
+	
+		// Number of mixing channels available for sound effect `Chunk`s to play
+		// simultaneously.
+		sdl2::mixer::allocate_channels(4);
+	    {
+			let n = sdl2::mixer::get_chunk_decoders_number();
+			println!("available chunk(sample) decoders: {}", n);
+			for i in 0..n {
+				println!("  decoder {} => {}", i, sdl2::mixer::get_chunk_decoder(i));
+			}
+		}
+	
+		{
+			let n = sdl2::mixer::get_music_decoders_number();
+			println!("available music decoders: {}", n);
+			for i in 0..n {
+				println!("  decoder {} => {}", i, sdl2::mixer::get_music_decoder(i));
+			}
+		}
+	
+
+		println!("query spec => {:?}", sdl2::mixer::query_spec());
+		let path = Path::new("./music/Rampage.mp3");
+
+		let music = sdl2::mixer::Music::from_file(path)?;
+		music.play(1);
 
 		// CREATE PLAYER SHOULD BE MOVED TO player.rs
 		let mut player = player::Player::new(
